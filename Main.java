4bc04f6e7c57674db2b5385075681e8dda8d59c4
apinan12 <apinan_za_za@hotmail.com>
2016-04-13 22:59:42 +0700

    #01 ได้หน้าต่าง GUI แล้ว

---
+++ Main.java
@@ -0,0 +1,20 @@
+package F2;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class Main{
+
+	public static void main(String[] args){
+		JFrame f = new JFrame("Space War");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(400, 650);
+		f.getContentPane().setLayout(new BorderLayout());
+		f.setVisible(true);
+
+	}
+
+
+
+}
